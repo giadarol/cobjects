9b2de02f4878105b6005ced40ed35abd6fa9448e
Riccardo De Maria <riccardodemaria@gmail.com>
2018-06-20 23:47:32 +0200

    update

---
+++ examples/buffer.py
@@ -0,0 +1,15 @@
+from cobjects import CBuffer
+
+b=CBuffer()
+b.info()
+
+b.new_object(24,2,[])
+b.info()
+obj=b.get_object_buffer(0).view('uint64')
+obj[0]=31
+
+b.reallocate(100,100,100,100)
+b.info()
+
+b._test_cffilib()
+
