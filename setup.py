cafd1c12f5f3b081a06a958f6b583a2264e3a27b
Riccardo De Maria <riccardodemaria@gmail.com>
2019-04-16 16:16:59 +0200

    fix build

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         python_requires='>=3.6',
         packages=['cobjects'],
         package_dir={'cobjects': 'cobjects'},
-        cffi_modules=["./_cffi_build/cbuffer_builder.py:ffibuilder"],
+        cffi_modules=["_cffi_build/cbuffer_builder.py:ffibuilder"],
         install_requires=['numpy','cffi>=1.0.0'],
         setup_requires=['cffi>=1.0.0']
 )
